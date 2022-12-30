33cde057c151d5271c6a435e6fdf87beddf405cb
Liam <therealbl4nc@gmail.com>
2022-12-31 03:22:45 +1100

    updated todo and new import

--- Interpreter/process.js
+++ Interpreter/process.js
@@ -1,6 +1,17 @@
-const { _Interpreter } = require('./class')
+import { _Interpreter } from './class.js'
 
-const I = new _Interpreter("SJS programs/fizzbuzz.sjs")
-I.runProgram() 
+const I = new _Interpreter("SJS programs/tutorial.sjs", 0)
+I.runProgram()
+// I.printVariables()
 
-I.printVariables()
+/**
+ * TODO:
+ * Fix tutorial and bracket handling!
+ * add functions
+ * add error handling
+ * bug fixing
+ * 
+ * maybe vscode extension
+ * maybe html ide and console
+ * 
+ */
