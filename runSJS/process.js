ee502ccf8853e5735ee1056e852db710d90bf7b7
Liam <therealbl4nc@gmail.com>
2023-01-18 01:06:46 +1100

    Add process.js

---
+++ runSJS/process.js
@@ -0,0 +1,16 @@
+import { _Interpreter } from '../Interpreter/class.js'
+
+const I = new _Interpreter("SJS programs/tutorial.sjs", 0)
+I.runProgram()
+
+/**
+ * TODO:
+ * Fix tutorial and bracket handling!
+ * add functions
+ * add error handling
+ * bug fixing
+ * 
+ * maybe vscode extension
+ * maybe html ide and console
+ * 
+ */
